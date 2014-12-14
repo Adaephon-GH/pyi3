b1f1537e10792bd6c40557569eac38e3ee140542
Adaephon-GH <adaephon@gmx.net>
2014-12-14 15:33:39 +0100

    start working on event queueing

--- pyi3.py
+++ pyi3.py
@@ -3,6 +3,7 @@
 __author__ = 'Adaephon'
 
 import json
+import queue
 import socket
 import struct
 import subprocess
@@ -56,13 +57,14 @@ class Socket:
         return msgType, data
 
     def receive(self):
-        msgType, data = self._receive()
-        isEvent = msgType >> 31
+        type_, data = self._receive()
+        isEvent = type_ >> 31
+        typeName = (eventTypes[type_ & 0x7f] if isEvent
+                    else msgTypes[type_])
         parsedData = json.loads(data.decode())
-        response = {'Origin': 'Event' if isEvent else 'Reply',
-                    'Type': eventTypes[msgType & 0x7f] if isEvent
-                    else msgTypes[msgType],
-                    'Payload': parsedData}
+        response = ('event' if isEvent else 'reply',
+                    typeName,
+                    parsedData)
         return response
 
     def __getattr__(self, attr):
@@ -76,20 +78,44 @@ class Socket:
                 self.__class__.__name__, attr))
 
 
-class Subscription:
+class EventHandler:
     def __init__(self, socket=None):
-        self.hooks = [[]] * len(eventTypes)
         self.socket = socket or Socket()
+        self.events = [0] * len(eventTypes)
+        self._eventqueue = queue.Queue()
+        self._subscript_confirmation = queue.Queue()
 
-    def register(self, hook):
-        self.hooks[eventTypesMap[hook.event]].append(hook)
+    def run(self):
+        self.isrunning = True
 
-    def initialize(self):
+    def _read_socket(self):
+        while True:
+            dataType, name, payload = self.socket.receive()
+            if dataType == 'event':
+                self._eventqueue.put((name, payload))
+            else if name == 'subcribe':
+                self._subscript_confirmation.put(payload)
+            else:
+                raise UnexpectedDataError((dataType, name, payload))
+
+    def _handle_events(self):
+        while True:
+            type_, payload = self._eventqueue.get()
+            self._handle_event(type_, payload)
+
+    def _handle_event(self, type_, payload):
         pass
-        self.socket.subscribe()
+        print(type_, payload)
 
+    # TODO: something to pause handling (certain) events to avoid recursion
+    def pause(self, events=None):
+        pass
 
 
 class Hook:
     def __init__(self, event, change=None, callback=None):
         pass
+
+
+class UnexpectedDataError(Exception):
+    pass
