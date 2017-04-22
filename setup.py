efd21b271c0510fa3ccd1d5081dcfbd407555e85
Adaephon-GH <adaephon@gmx.net>
2017-04-22 23:07:29 +0200

    Make it a proper python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyi3',
+    version='0.1.dev1',
+    packages=['pyi3'],
+    url='https://github.com/Adaephon-GH/pyi3',
+    license='MIT',
+    author='Adaephon',
+    author_email='adaephon@gmx.net',
+    description='IPC-interface for the i3 window manager'
+)
